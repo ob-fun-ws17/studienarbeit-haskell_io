f26e065b57c5869f091b72383257f27bd8b474e0
Andreas <andreas@mosix.de>
2017-12-27 18:53:11 +0100

    Added some funktionality to the Cmd module. (Comands: help, quit)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
-
-import Lib
+import System.Environment
+import Cmd
 
 main :: IO ()
-main = someFunc
+main = do
+      arguments <- getArgs
+      if length arguments == 1 && (head arguments) == "--gui"
+        then putStrLn "start the GUI! -- Which GUI? -- OUR GUI! -- We have no GUI! -- ok ... sorry..."
+
+        else
+             cmdMain
--- src/Cmd.hs
+++ src/Cmd.hs
@@ -1,3 +1,35 @@
-module Cmd
-    ( 
-    ) where
+module Cmd (cmdMain) where
+
+import System.IO (hSetBuffering, BufferMode(NoBuffering), stdout)
+
+cmdMain :: IO ()
+cmdMain = do
+  hSetBuffering stdout NoBuffering
+  putStr "Welcome to the JPEG RAW Organizer \nType \"help\" to see all commands\n"
+  inputHandler
+
+inputHandler :: IO ()
+inputHandler = do
+  hSetBuffering stdout NoBuffering
+  input <- readLn :: IO String
+  handleCommand input
+
+
+handleCommand :: String -> IO ()
+handleCommand "quit" = putStrLn "Beenden..."
+handleCommand "help" = do
+  printHelp
+  inputHandler
+handleCommand cmd = do
+  putStrLn ("! Unknown Command: " ++ cmd)
+  printHelp
+  inputHandler
+
+
+printHelp :: IO ()
+printHelp = do
+  putStrLn ("-------------------------------------------")
+  putStrLn ("HELP:")
+  putStrLn ("+ Type \"quit\" to leave the program.")
+  putStrLn ("+ Type \"help\" for help.")
+  putStrLn ("-------------------------------------------")
