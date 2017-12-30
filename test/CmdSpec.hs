0d07563a51f9f47a8d8cf1bd16e6fe08eed59087
christoph <christophsuerig@googlemail.com>
2017-12-30 10:41:43 +0100

    added uniform... tests for cmd

--- test/CmdSpec.hs
+++ test/CmdSpec.hs
@@ -1,16 +1,40 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module LibSpec (spec) where
 
-import Lib (square)
+import Cmd (uniformFileExtension, uniformFilePath)
 import Test.Hspec
 import Test.QuickCheck
 
 spec :: Spec
+-- | tests for uniformFileExtension
 spec =
-describe "square" $ do
-it "calculates the square of 5.3" $
-square 5.3 `shouldBe` 28.09
-it "calculates the square of an arbitrary integer" $
-property $ \(n :: Integer) -> square n == n * n
-it "calculates the square of an arbitrary double" $
-property $ \(n :: Double) -> square n == n * n
+describe "uniformFileExtension" $ do
+it "uniformes the file extension jpg" $
+uniformFileExtension "jpg" `shouldBe` ".jpg"
+it "uniformes the file extension .jpg" $
+uniformFileExtension ".jpg" `shouldBe` ".jpg"
+it "uniformes the empty file extension" $
+uniformFileExtension "" `shouldBe` ".*"
+-- | tests for uniformFilePath
+spec =
+describe "uniformFilePath" $ do
+it "uniforms the path C:/user/noUser" $
+uniformFilePath "C:/user/noUser" `shouldBe` "C:/user/noUser/"
+it "uniforms the path C:\\user\\noUser" $
+uniformFilePath "C:\\user\\noUser" `shouldBe` "C:/user/noUser/"
+it "uniforms the path C:/user/noUser/" $
+uniformFilePath "C:/user/noUser/" `shouldBe` "C:/user/noUser/"
+it "uniforms the path C:\\user\\noUser\\" $
+uniformFilePath "C:\\user\\noUser\\" `shouldBe` "C:/user/noUser/"
+it "uniforms the empty path " $
+uniformFilePath "" `shouldBe` "./"
+
+-- | tests for uniformFileExtension
+-- spec =
+-- describe "uniformFileExtension" $ do
+-- it "uniformes the file extension jpg" $
+-- uniformFileExtension "jpg" `shouldBe` ".jpg"
+-- it "uniformes the file extension .jpg" $
+-- uniformFileExtension ".jpg" `shouldBe` ".jpg"
+-- it "uniformes the empty file extension" $
+-- uniformFileExtension "" `shouldBe` ".*"
